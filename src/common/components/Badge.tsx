6d134c234cc683c3ef16b7eebfc2337240eb6c8b
night-fury-rider <5191208+night-fury-rider@users.noreply.github.com>
2024-09-29 16:51:42 +0530

    Improvised Badge component.

--- src/common/components/Badge.tsx
+++ src/common/components/Badge.tsx
@@ -1,17 +1,18 @@
 import {Badge as LibraryBadge, useTheme} from 'react-native-paper';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, ViewStyle} from 'react-native';
 import {View} from 'react-native';
 
 type BadgeProps = {
   value: string | number;
+  customStyles?: ViewStyle;
 };
 
-const Badge = ({value}: BadgeProps) => {
+const Badge = ({value, customStyles}: BadgeProps) => {
   const theme = useTheme();
   const styles = getStyles(theme.colors.primary);
   return (
     <View style={styles.container}>
-      <LibraryBadge style={styles.badge} size={40}>
+      <LibraryBadge style={[styles.badge, customStyles]} size={40}>
         {value}
       </LibraryBadge>
     </View>
