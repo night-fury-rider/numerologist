85fe7f112e8ae72fbe0fd725ac8de8afd3e9ebf6
Yuvraj Patil <5191208+night-fury-rider@users.noreply.github.com>
2026-09-23 21:30:25 +0530

    Fixed loose references of NameNumerology.

--- ios/NameNumerologyTests/NameNumerologyTests.m
+++ ios/NameNumerologyTests/NameNumerologyTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface NameNumerologyTests : XCTestCase
+@interface NumerologistTests : XCTestCase
 
 @end
 
-@implementation NameNumerologyTests
+@implementation NumerologistTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
